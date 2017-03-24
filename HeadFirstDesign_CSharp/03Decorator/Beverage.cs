69475f7758ebd1a40d681a565132b00cc79a6174
zhoukaibing <871041532@outlook.com>
2017-03-24 18:33:46 +0800

    Decorator
    Decorator

---
+++ HeadFirstDesign_CSharp/03Decorator/Beverage.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03Decorator
+{
+    public abstract class Beverage
+    {
+        protected string description="Unkown Beverage";
+        public void setDescription(string description) { this.description = description; }
+        public string getDescription() { return description; }
+        public abstract double cost();
+    }
+    //四种
+    class DarkRoast : Beverage
+    {
+        public DarkRoast() { description = "DarkRoast coffee"; }
+        public override double cost()
+        {
+            return 3.2;
+        }
+    }
+    class HouseBlend : Beverage
+    {
+        public HouseBlend() { description = "HouseBlend coffee"; }
+        public override double cost()
+        {
+            return 5.65;
+        }
+    }
+    class Espresso : Beverage
+    {
+        public Espresso() { description = "Espresso coffee"; }
+        public override double cost()
+        {
+            return 1.99;
+        }
+    }
+    class Decaf : Beverage
+    {
+        public Decaf() { description = "Decaf coffee"; }
+        public override double cost()
+        {
+            return 3.45;
+        }
+    }
+    //调味
+    public abstract class CondimentDecorator:Beverage
+    {
+        public Beverage bevarage;
+        public abstract new string getDescription();
+    }
+    //继承
+    class Milk : CondimentDecorator
+    {     
+        public Milk(Beverage bevarage)
+        {
+            this.bevarage = bevarage;
+        }
+        public override double cost()
+        {
+            return 0.20 + bevarage.cost();
+        }
+
+        public override string getDescription()
+        {
+            return bevarage.getDescription() + ",Mocha";
+        }
+    }
+    class Mocha : CondimentDecorator
+    {
+        public override double cost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string getDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Soy : CondimentDecorator
+    {
+        public override double cost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string getDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Whlp : CondimentDecorator
+    {
+        public override double cost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string getDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HeadFirstDesign_CSharp/03Decorator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*开闭原则：类应该对扩展开放，对修改关闭
+ * 在选择需要被扩展的代码部分时要小心。
+ * 每个地方都采用开闭原则是一种浪费，没必要，还会导致代码变得复杂且难以理解。
+ */
+/*装饰者模式：动态地将责任附加到对象上。
+ * 若要扩展功能，装饰者提供了比继承更要弹性的替代方案。
+ */
+namespace _03Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
