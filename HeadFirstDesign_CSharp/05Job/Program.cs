9a2cbf6b6db70893296063dbe9e64595cd0c01c6
zhoukaibing <871041532@outlook.com>
2017-04-26 10:00:40 +0800

    事务模式
    事务模式

---
+++ HeadFirstDesign_CSharp/05Job/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05Job
+{
+    //Job模式
+    //单线程异步解决方案，将待完成的事务逻辑(JOB)划分为树形结构同步或异步执行
+    //优点:使用方便功能强大，不依赖特定语言特性通用性强
+    //缺点:大量使用lambda回调，出现错误较难定位。
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
