13d7d6c08dfbf1847a8348d28f309ee604497d55
871041532 <871041532@outlook.com>
2017-09-01 18:36:25 +0800

    代理模式加了个字
    代理模式加了个字

---
+++ HeadFirstDesign_CSharp/14Proxy/GumballMachine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14Proxy
+{
+    public class GumballMachine
+    {
+        string location;
+    }
+}
